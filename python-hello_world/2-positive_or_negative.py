c448ec149ada218ef96a7488361931c9423600f7
soumyaranjanbhanja <soumyaranjanb@brightcomgroup.com>
2022-12-23 13:12:27 +0000

    2

--- python-hello_world/2-positive_or_negative.py
+++ python-hello_world/2-positive_or_negative.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 import random
-number=random.randint(-10,20)
-if number>0:
-       print("is positive")
-elif number==0:
-       print("is zero")
+number = random.randint(-10, 10)
+if number > 0:
+    print("is positive")
+elif number == 0:
+    print("is zero")
 else:
-       print("is negative")
+    print("is negative")
